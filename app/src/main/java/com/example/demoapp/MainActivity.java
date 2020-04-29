b8bf687b2734a1069407e66d9d8ddaf30c0427a2
NeutralDan <64358179+NeutralDan@users.noreply.github.com>
2020-04-29 12:56:00 +0100

    Empty App - Ready for testing!

--- app/src/main/java/com/example/demoapp/MainActivity.java
+++ app/src/main/java/com/example/demoapp/MainActivity.java
@@ -2,14 +2,44 @@ package com.example.demoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    int answer = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Random rand = new Random();
+        answer = rand.nextInt(20) + 1;
     }
 
+    public void guessSubmitted (View view) {
+
+        EditText guessEditText = findViewById(R.id.guessEditText);
+        int guess = Integer.parseInt(guessEditText.getText().toString());
+
+        if (guess > answer) {
+                createToast("Lower");
+            }
+        else if (guess < answer) {
+                createToast("Higher");
+        }
+        else {
+            createToast("Thats the one! Go again");
+            Random rand = new Random();
+            answer = rand.nextInt(20) +1;
+        }
+    }
+
+    public void createToast(String message) {
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
 }
